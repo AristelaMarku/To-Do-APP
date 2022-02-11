9a498f301197de2ec9a5b51185f9aea45a5bebc6
Aristela Marku <aristela.marku@gmail.com>
2022-02-10 22:07:21 -0500

    update functionality finished

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -5,16 +5,17 @@ function App() {
    const [inpotForm,setInputForm]=useState()
    const [task, settask]=useState([])
    const [show, setShow] = useState({ onetask: false })
-
+   const [update, setUpdate]=useState("")
+   const [updateForm, setUpdateForm] = useState()
+   
+   console.log(update.id)
+   console.log(updateForm)
    useEffect(()=>{
     fetch ("http://localhost:3000/todo")
     .then(res=>res.json())
     .then(data => settask(data))
    },[])
 
-  //  const addNewTask = (newTask) =>{
-  //   settask([...task, newTask])
-  //  }
 
    const handleSubmit = (e) => {
      e.preventDefault() 
@@ -46,20 +47,39 @@ function App() {
    }
 
    const handleUpdate = (taskToUpdate) =>{
+     console.log(taskToUpdate)
      setShow({[taskToUpdate.id]: !show.point})
+     setUpdate(taskToUpdate) 
+     setUpdateForm(taskToUpdate.task)
+   }
 
-     fetch(`http://localhost:3000/todo/${taskToUpdate.id}`,{
-       method: "PATCH",
-       headers:{
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        task:taskToUpdate
-      })
-      .then(res=>res.json())
-      .then(data =>console.log(data))
+  
+
+   const handleUpdateSubmit = (e) =>{
+    e.preventDefault() 
+    fetch(`http://localhost:3000/todo/${update.id}`,{
+      method: "PATCH",
+      headers:{
+       'Accept': 'application/json',
+       'Content-Type': 'application/json'
+     },
+     body: JSON.stringify({
+       task:updateForm
+     })
     })
+     .then(res=>res.json())
+     .then(data => {
+       const taskToRenderr= task.map((onetask)=>{
+         if(onetask.id === data.id){
+            return data
+         }else{
+           return onetask
+         }
+       })
+       settask(taskToRenderr)
+     })
+     setShow({[updateForm.id]: !show.point})
+     e.target.reset();
    }
 
    const taskTorender = task.map((onetask)=>{
@@ -78,8 +98,8 @@ function App() {
     : 
     (
     <>
-    <form>
-        <input/>
+    <form onSubmit={handleUpdateSubmit}>
+        <input type="text" name="todo" value={updateForm} onChange={(e)=>setUpdateForm(e.target.value)}/>
         <button>update</button>
     </form>
     </>
